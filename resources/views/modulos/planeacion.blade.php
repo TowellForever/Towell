230001ef79ef9300cd87f08101f0a376f6ef0321
Juan Jesus Álvarez Rodriguez <jealvarez@SISTEMAS.LOCAL>
2025-05-12 13:41:46 -0600

    style(planeacion): elimina campos innecesarios

--- resources/views/modulos/planeacion.blade.php
+++ resources/views/modulos/planeacion.blade.php
@@ -155,9 +155,6 @@
                       </tr>
                   </thead>
                   <tbody>
-                      <!--<tr id="filaInicio" class="bg-gray-100 text-gray-700"><td class="border px-2 py-1">Inicio</td></tr>
-                      <tr id="filaFin" class="bg-white text-gray-800"><td class="border px-2 py-1">Fin</td></tr>-->
-                      <tr id="filaFraccion" class="bg-gray-100 text-gray-700"><td class="border px-2 py-1">Fracción Día</td></tr>
                       <tr id="filaPzas" class="bg-white text-gray-800"><td class="border px-2 py-1">Pzas</td></tr>
                       <tr id="filaKilos" class="bg-gray-100 text-gray-700"><td class="border px-2 py-1">Kilos</td></tr>
                       <tr id="filaTej" class="bg-white text-gray-800"><td class="border px-2 py-1">Tejedor</td></tr>
@@ -304,9 +301,6 @@
         document.addEventListener("DOMContentLoaded", function () {
             const hoy = new Date();
             const filaFechas = document.getElementById("filaFechas");
-            const filaInicio = document.getElementById("filaInicio");
-            const filaFin = document.getElementById("filaFin");
-            const filaFraccion = document.getElementById("filaFraccion");
             const filaPzas = document.getElementById("filaPzas");
             const filaKilos = document.getElementById("filaKilos");
             const filaTej = document.getElementById("filaTej");
@@ -323,9 +317,6 @@
 
                 // Celdas vacías por fecha
                 filaFechas.innerHTML += `<th class="border border-gray-300">${fechaFormateadaDiaMes}</th>`;
-                //filaInicio.innerHTML += `<td class="border border-gray-300"></td>`;
-                //filaFin.innerHTML += `<td class="border border-gray-300"></td>`;
-                filaFraccion.innerHTML += `<td class="border border-gray-300"></td>`;
                 filaPzas.innerHTML += `<td class="border border-gray-300"></td>`;
                 filaKilos.innerHTML += `<td class="border border-gray-300"></td>`;
                 filaTej.innerHTML += `<td class="border border-gray-300"></td>`;
@@ -348,7 +339,7 @@
                         .then(res => res.json())
                         .then(data => {
                             // Limpiar, quite estas: filaInicio, filaFin,
-                            [ filaFraccion, filaPzas, filaKilos, filaTej].forEach(row => {
+                            [  filaPzas, filaKilos, filaTej].forEach(row => {
                                 row.querySelectorAll("td:not(:first-child)").forEach(cell => cell.textContent = "");
                             });
 
@@ -357,15 +348,10 @@
                                 const fechaItemDiaMes = fechaItem.slice(5, 10); // "05-12"
                                 const index = fechas.indexOf(fechaItem);
                                 if (index !== -1) {
-                                    //filaInicio.cells[index + 1].textContent = item.fecha_inicio.slice(5, 10);
-                                    //filaFin.cells[index + 1].textContent = item.fecha_fin.slice(5, 10);
-                                    filaFraccion.cells[index + 1].textContent = item.fraccion_dia;
                                     filaPzas.cells[index + 1].textContent = item.pzas;
                                     filaKilos.cells[index + 1].textContent = item.kilos;
                                     filaTej.cells[index + 1].textContent = item.tej_num;
                                 }
-                            //console.log("Fecha del item:", fechaItem);
-                            //console.log("Índice encontrado:", index);
 
                             
                               });
